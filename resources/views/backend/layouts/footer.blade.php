a2ae4273669f82b808808dcee4a503a0b10baaff
Razib Hasan <workrazib@gmail.com>
2023-05-24 11:06:32 +0600

    footnote changed

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -2,11 +2,11 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                 EasyLearning © Upcube.
+                                 razib.dev © Upcube.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by EasyLearning
+                                    Developed by <i class="mdi mdi-heart text-danger"></i> razib.dev
                                 </div>
                             </div>
                         </div>
